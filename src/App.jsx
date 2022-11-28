0501fd841770070eb9254633a1248ef5d9c60316
juliasavicke <juliasavicke@gmail.com>
2022-11-28 08:35:25 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div className='App container'>
+      <h1>React</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
