da93603d279b1115276fcad17e79795853da258a
juliasavicke <juliasavicke@gmail.com>
2022-11-28 09:51:22 +0200

    search done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,51 @@
+import { useState } from 'react';
+
 function Search() {
-  return <div>Search</div>;
+  const [searchValue, setSearchValue] = useState('');
+  const [searchDone, setSearchDone] = useState(false);
+
+  //padaryti kad veiktu isSearchValueEmpty
+
+  let isSearchValueEmpty = !searchValue.length;
+  console.log('isSearchValueEmpty ===', isSearchValueEmpty);
+
+  function inputHandler(event) {
+    //one way binding
+    const enteredValue = event.target.value.trim();
+    // if (enteredValue === '') return;
+    setSearchValue(enteredValue);
+
+    setSearchDone(false);
+  }
+
+  //   let completeSearch = '';
+  const [completeSearch, setcompleteSearch] = useState('');
+
+  function searchHandler() {
+    // when user pressed search
+    // siusti paieskos objekta, {term: <paieskos reiksme>}
+    let searchObj = { term: searchValue };
+    console.log('searchObj ===', searchObj);
+    // isvalyti paieskos lauka
+    setSearchValue('');
+    // atvaizduoti ko ieskojome
+    setSearchDone(true);
+    setcompleteSearch(searchObj.term);
+  }
+
+  //   console.log('searchValue ===', searchValue);
+  return (
+    <fieldset>
+      <legend>Search</legend>
+      {/** //two way binding */}
+      <input onChange={inputHandler} type='search' value={searchValue} />
+      <button onClick={searchHandler}>Search</button>
+      {/** rodyti priklausomai nuo isSearchValueEmpty ir ideji i ko ieskojau */}
+      {!isSearchValueEmpty && <h3>You have entered: {searchValue}</h3>}
+      {searchDone && isSearchValueEmpty && (
+        <h3>Your search is done {completeSearch}</h3>
+      )}
+    </fieldset>
+  );
 }
 export default Search;
